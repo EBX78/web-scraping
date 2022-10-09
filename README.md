5414e94e59ee5cb78a39e779b29cc7345e5241f2
Abdullah Amiri <abdullah.7901741@gmail.com>
2022-10-09 12:52:21 +0330

    100 to 75 line using short if statement

---
+++ README.md
@@ -0,0 +1,17 @@
+# web-scraping
+
+* Data extraction from the site
+* Sort it out
+* Select and receive the necessary data
+* Adding data to the database
+* Use the data for ml tasks :)
+
+### Before running, create a table with these specifications
+
+| Field    | Type          | 
+|----------|---------------|
+| name     | varchar(200)  |
+| type     | varchar(25)   |
+| sex      | char(1)       |
+| page     | int           |
+| price    | varchar(15)   |
--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import requests                 # requests for site data
 from bs4 import BeautifulSoup   # readability of site data
 from unidecode import unidecode # persian number format to latin format
 
-def PRICE(price_text):                 # remove str from price
+# connect to database, create cursor, the command for insert data to database
+cnx = pymysql.connect(user = 'root', password = '795L_5795l_5', host = '127.0.0.1', database = 'abdullah')
+cursor = cnx.cursor()
+sql = 'INSERT INTO perfume VALUES ("%s", "%s", "%s", "%s", "%s")'
+
+# remove strings from price integer
+def PRICE(price_text):
     List = price_text.text.split()
     shits = ["تومان", "از"]
     for shit in shits:
@@ -12,90 +18,58 @@ def PRICE(price_text):                 # remove str from price
 
     return unidecode(List[0].replace(",", ""))
 
-cnx = pymysql.connect(user = 'root', password = '795L_5795l_5', host = '127.0.0.1', database = 'abdullah')  # connect to database
-cursor = cnx.cursor()                                                                                       # create cursor
-sql = 'INSERT INTO perfume VALUES ("%s", "%s", "%s", "%s", "%s")'                                           # the command for insert data to database
+# type of items(perfumes), inserted to database (All) & existing counter (existing)
+par, edp, edt, edc, none, All, existing = 1, 2, 3, 4, 0, 0, 0
 
-s_page = int(input("START IN: "))       # start in page N
-e_page = int(input("FINISH IN: "))      # finish in page N
-
-par = 1     # type of items(perfume)
-edp = 2
-edt = 3
-edc = 4
-none = 0
-
-selected = 0    # inserted item to database
-existing = 0    # count existing
+# start and finish in page N
+s_page = int(input("START IN: "))
+e_page = int(input("FINISH IN: "))
 
 # for each page
 while s_page != e_page:
-    response = requests.get("https://rojashop.com/shop/fragrances/eudoclon?sub=eudoclon&page=%d"% s_page)   # request data
-    print(f"PAGE:{s_page}")                                                                                # print page number
-    soup = BeautifulSoup(response.text, "html.parser")                                                      # readable site data
-    name = soup.find_all("strong", attrs = {"class" : "ptitle"})                                            # find items name
-    price = soup.find_all("span", attrs = {"class" : "sale-price"})                                         # find items price
-    i = 0                                                                                                   # index counter for PRICE (line 45)
+    print(f"PAGE:{s_page}")
+    # request site data, read site data, find item names, find items price
+    response = requests.get("https://rojashop.com/shop/fragrances/eudoclon?sub=eudoclon&page=%d"% s_page)
+    soup = BeautifulSoup(response.text, "html.parser")
+    name = soup.find_all("strong", attrs = {"class" : "ptitle"})
+    price = soup.find_all("span", attrs = {"class" : "sale-price"})
 
-    for item in name:       # name of all perfume in s_page
-        sent = item.text        # sent variable for find type and sex for items (names)
+    # perfumes in page {s_page}
+    for i, item in enumerate(name):
+        # sent variable is for find type and sex in items, item counter
+        sent = item.text
+        All += 1
 
         try:
-            r_price = PRICE(price[i])   # price value in (i) index (price line 38)
-            existing += 1               # count existing
+            # cleaning price value, existing counter
+            r_price = PRICE(price[i])
+            existing += 1
         except IndexError:
-            r_price = "---"             # ... if price value not exist in (i) index (price line 38)
-        finally:
-            # print(r_price)
-            i += 1                      # next index
+            r_price = "---"
 
-        if "ادو" in sent and "تویلت" in sent:                               # type
-            if "مردانه" in sent:                                            # sex
-                cursor.execute(sql % (sent, edt, 1, s_page, r_price))       # execute and insert data
-            elif "زنانه" in sent:
-                cursor.execute(sql % (sent, edt, 2, s_page, r_price))
-            else:
-                cursor.execute(sql % (sent, edt, 0, s_page, r_price))
+        # par
+        if "ادو" not in sent and "پرفیوم" in sent:
+            cursor.execute(sql % (sent, par, 1, s_page, r_price)) if "مردانه" in sent else cursor.execute(sql % (sent, par, 2, s_page, r_price)) if "زنانه" in sent else cursor.execute(sql % (sent, par, 0, s_page, r_price))
 
-
+        # edp
         elif "ادو" in sent and "پرفیوم" in sent:
-            if "مردانه" in sent:
-                cursor.execute(sql % (sent, edp, 1, s_page, r_price))
-            elif "زنانه" in sent:
-                cursor.execute(sql % (sent, edp, 2, s_page, r_price))
-            else:
-                cursor.execute(sql % (sent, edp, 0, s_page, r_price))
+            cursor.execute(sql % (sent, edp, 1, s_page, r_price)) if "مردانه" in sent else cursor.execute(sql % (sent, edp, 2, s_page, r_price)) if "زنانه" in sent else cursor.execute(sql % (sent, edp, 0, s_page, r_price))
 
+        # edt
+        elif "ادو" in sent and "تویلت" in sent:
+            cursor.execute(sql % (sent, edt, 1, s_page, r_price)) if "مردانه" in sent else cursor.execute(sql % (sent, edt, 2, s_page, r_price)) if "زنانه" in sent else cursor.execute(sql % (sent, edt, 0, s_page, r_price))
 
-        elif "ادو" not in sent and "پرفیوم" in sent:
-            if "مردانه" in sent:
-                cursor.execute(sql % (sent, par, 1, s_page, r_price))
-            elif "زنانه" in sent:
-                cursor.execute(sql % (sent, par, 2, s_page, r_price))
-            else:
-                cursor.execute(sql % (sent, par, 0, s_page, r_price))
-
-
+        # edc
         elif ("کلون" in sent and "ادو" in sent) or ("کلون" in sent):
-            if "مردانه" in sent:
-                cursor.execute(sql % (sent, edc, 1, s_page, r_price))
-            elif "زنانه" in sent:
-                cursor.execute(sql % (sent, edc, 2, s_page, r_price))
-            else:
-                cursor.execute(sql % (sent, edc, 0, s_page, r_price))
+            cursor.execute(sql % (sent, edc, 1, s_page, r_price)) if "مردانه" in sent else cursor.execute(sql % (sent, edc, 2, s_page, r_price)) if "زنانه" in sent else cursor.execute(sql % (sent, edc, 0, s_page, r_price))
 
-
-        elif "مردانه" in sent:
-            cursor.execute(sql % (sent, none, 1, s_page, r_price))
-        elif "زنانه" in sent:
-            cursor.execute(sql % (sent, none, 2, s_page, r_price))
+        # none
         else:
-            cursor.execute(sql % (sent, none, 0, s_page, r_price))
+            cursor.execute(sql % (sent, none, 1, s_page, r_price)) if "مردانه" in sent else cursor.execute(sql % (sent, none, 2, s_page, r_price)) if "زنانه" in sent else cursor.execute(sql % (sent, none, 0, s_page, r_price))
 
-        cnx.commit()                                                        # run execute
-        selected += 1                                                       # count items
-    s_page += 1                                                             # next page
+        # excute to database
+        cnx.commit()
+    s_page += 1
 
 cnx.close()
-print("ALL:  %d"%selected)
-print("EXISTING:  %d"%existing)
+print(f'ALL: {All}\nEXISTING: {existing}')
